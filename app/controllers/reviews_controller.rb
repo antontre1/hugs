633c8ac00bb205587294a70c81056f71fe1781f3
Charles De Freminville <cdefreminville@lucca.fr>
2022-01-20 21:35:09 +0100

    reviews stars

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -32,7 +32,7 @@ class ReviewsController < ApplicationController
   end
 
   def show
-    @review = Review.find(params[:id])
+    @reviews = Review.find(params[:id])
     @hug = Hug.find(params[:hug_id])
     authorize @review
   end
