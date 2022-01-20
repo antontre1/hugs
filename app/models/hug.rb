f7921d36bf392dd40d7ba82f449219beee37e408
Antonin Folliasson <antonin@aitio.co>
2022-01-20 11:50:54 +0100

    formulaires ok pour hugs

--- app/models/hug.rb
+++ app/models/hug.rb
@@ -3,7 +3,7 @@ class Hug < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  validates :title, :description, :category, :price, :address, presence: true
+  validates :title, :description, :category, :price, :address, :photo, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
